0f004a67c2cf6743b017014b128a282077c35015
Bruno Dechen <brunodechen@Brunos-MacBook-Pro.local>
2024-03-14 08:18:31 -0300

    Full Functional login Page

--- prisma/data/users.ts
+++ prisma/data/users.ts
@@ -2,13 +2,13 @@ export const Users = [
     {
         name: 'Internal Service',
         email: 'internalservice@eletroequip.com.br',
-        password: '$2b$10$VwHek082rm1VFZCJFpCgP.OMu7caw0s8Qd.G84mcS5zd8P.Dmb7QO',
+        hashPassword: '$2b$10$VwHek082rm1VFZCJFpCgP.OMu7caw0s8Qd.G84mcS5zd8P.Dmb7QO',
         admin: true
     },
     {
         name: 'BRUNO DECHEN',
         email: 'bruno.dechen@eletroequip.com.br',
-        password: '$2b$10$9jhnsl7I2Yqb5P5v9Z2LrOo0c.c3eCxzXkV/cUSPn9gwQqnOnJpve',
+        hashPassword: '$2b$10$9jhnsl7I2Yqb5P5v9Z2LrOo0c.c3eCxzXkV/cUSPn9gwQqnOnJpve',
         admin: true
     },
 ]
